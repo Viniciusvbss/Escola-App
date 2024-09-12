73669fb46c1fe7ec13a8f6529c81a7f0e98a293e
Vinicius Barbosa Soares da Silva <viniciusdl.soares@gmail.com>
2024-09-12 13:51:44 -0300

    Corrige o navbar no mobile

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./component/header/header.component";
 import { UsuarioComponent } from "./component/usuario/usuario.component";
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UsuarioComponent],
+  imports: [RouterOutlet, HeaderComponent, UsuarioComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
